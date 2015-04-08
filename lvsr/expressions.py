8300a31d1be5977fa034dca1b4c9505520193085
rizar <dimabv@tut.by>
2015-04-08 20:13:58 +0200

    welcome 1D convolution!

--- lvsr/expressions.py
+++ lvsr/expressions.py
@@ -1,4 +1,5 @@
 from theano import tensor
+from theano.tensor.nnet import conv2d
 
 def weights_std(weights, mask_outputs=None):
     positions = tensor.arange(weights.shape[2])
@@ -20,3 +21,29 @@ def entropy(weights, mask_x):
     entropies = (weights * tensor.log(weights + 1e-7)).sum(axis=2)
     entropies *= mask_x
     return entropies.sum()
+
+def conv1d(sequences, masks, **kwargs):
+    """Wraps Theano conv2d to perform 1D convolution.
+
+    Parameters
+    ----------
+    sequence : :class:`~theano.Variable`
+        (batch_size, length)
+    masks : :class:`~theano.Variable`
+        (num_filters, filter_length)
+    **kwargs
+        Will be passed to `conv2d`
+
+    Returns
+    -------
+    result : :class:`~theano.Variable`
+        (batch_size, num_filters, position)
+
+    """
+    # For testability
+    sequences = tensor.as_tensor_variable(sequences)
+    masks = tensor.as_tensor_variable(masks)
+    image = sequences.dimshuffle(0, 'x', 1, 'x')
+    filters = masks.dimshuffle(0, 'x', 1, 'x')
+    return tensor.addbroadcast(
+        conv2d(image, filters, **kwargs), 3).squeeze()
