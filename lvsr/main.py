4d2f1b75a9c7eef3c01af1a86f0903ee11355d95
rizar <dimabv@tut.by>
2015-04-10 08:14:20 +0000

    Make attentions without energies work

--- lvsr/main.py
+++ lvsr/main.py
@@ -377,13 +377,16 @@ class SpeechRecognizer(Initializable):
         if not hasattr(self, "_analyze"):
             cost = self.get_cost_graph(batch=False)
             cg = ComputationGraph(cost)
-            energies, = VariableFilter(
+            energies = VariableFilter(
                 bricks=[self.generator], name="energies")(cg)
+            energies_output = [energies[0][:, 0, :] if energies
+                               else tensor.zeros((self.single_transcription.shape[0],
+                                                  self.single_recording.shape[0]))]
             weights, = VariableFilter(
                 bricks=[self.generator], name="weights")(cg)
             self._analyze = theano.function(
                 [self.single_recording, self.single_transcription],
-                [cost[:, 0], weights[:, 0, :], energies[:, 0, :]])
+                [cost[:, 0], weights[:, 0, :]] + energies_output)
         return self._analyze(recording, transcription)
 
     def init_beam_search(self, beam_size):
