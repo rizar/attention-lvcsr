f900c43e5a847a9e4050880910e14360bd801bcb
rizar <dimabv@tut.by>
2015-04-15 12:07:24 +0200

    Adaptive gradient clipping

--- lvsr/extensions.py
+++ lvsr/extensions.py
@@ -2,10 +2,11 @@
 from __future__ import print_function
 import subprocess
 import pkgutil
+import math
 
 from theano.scan_module.scan_op import Scan
 
-from blocks.extensions import SimpleExtension
+from blocks.extensions import TrainingExtension, SimpleExtension
 
 class CGStatistics(SimpleExtension):
 
@@ -49,3 +50,31 @@ class IPDB(SimpleExtension):
     def do(self, *args, **kwargs):
         import ipdb; ipdb.set_trace()
 
+
+class AdaptiveClipping(TrainingExtension):
+
+    def __init__(self, log_record, clipping_rule,
+                 initial_threshold, burnin_period=100, decay_rate=0.99):
+        self.log_record = log_record
+        self.clipping_rule = clipping_rule
+        self.initial_threshold = initial_threshold
+        self.burnin_period = burnin_period
+        self.decay_rate = decay_rate
+
+        self.mean_gradient_norm = self.mean_gradient_norm2 = .0
+
+    def after_batch(self, batch):
+        gradient_norm = math.log(self.main_loop.log.current_row[self.log_record])
+        self.mean_gradient_norm = (self.decay_rate * self.mean_gradient_norm
+                                   + (1 - self.decay_rate) * gradient_norm)
+        self.mean_gradient_norm2 = (self.decay_rate * self.mean_gradient_norm2
+                                    + (1 - self.decay_rate) * gradient_norm ** 2)
+        self.std_gradient_norm = (
+            (self.mean_gradient_norm2 - self.mean_gradient_norm ** 2) ** .5)
+        threshold = math.exp(self.mean_gradient_norm + 1 * self.std_gradient_norm)
+        confidence = (min(
+            self.burnin_period, self.main_loop.status['iterations_done']) /
+            float(self.burnin_period))
+        threshold = (confidence * threshold +
+                     (1 - confidence) * self.initial_threshold)
+        self.clipping_rule.threshold.set_value(threshold)
