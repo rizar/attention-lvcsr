f6eb868fd8d4192677dea020f3493019cf42c789
rizar <dimabv@tut.by>
2015-05-27 09:42:34 +0000

    Fix average weight norms

--- lvsr/main.py
+++ lvsr/main.py
@@ -776,9 +776,9 @@ def main(cmd_args):
             clipping.threshold]
         for name, param in params.items():
             num_elements = numpy.product(param.get_value().shape)
-            norm = param.norm(2) / num_elements
-            grad_norm = algorithm.gradients[param].norm(2) / num_elements
-            step_norm = algorithm.steps[param].norm(2) / num_elements
+            norm = param.norm(2) / num_elements ** 0.5
+            grad_norm = algorithm.gradients[param].norm(2) / num_elements ** 0.5
+            step_norm = algorithm.steps[param].norm(2) / num_elements ** 0.5
             stats = tensor.stack(norm, grad_norm, step_norm, step_norm / grad_norm)
             stats.name = name + '_stats'
             observables.append(stats)
