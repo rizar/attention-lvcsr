f06fa55b61fdcf7a58fcef51944446d6c12f104c
rizar <dimabv@tut.by>
2015-06-26 18:01:41 +0000

    Filter end labels when decoding

--- lvsr/datasets/h5py.py
+++ lvsr/datasets/h5py.py
@@ -14,6 +14,7 @@ class H5PYAudioDataset(H5PYDataset):
 
 
     def decode(self, labels):
-        return "".join(self.num2char[label] for label in labels)
+        return "".join(self.num2char[label] for label in labels
+                       if label != self.eos_label)
 
 
