188f8024ebbfa06853c7cc8a85b74a3d5bbce4ff
Jan Chorowski <jan.chorowski@gmail.com>
2015-06-27 11:11:16 +0200

    correct env

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # The directory where the script is
-LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #python modules
-export PYTHONPATH=$LVSR/libs/blocks:$LVSR/libs/blocks-extras:$LVSR/libs/fuel:$LVSR/libs/Theano:$LVSR/libs/picklable-itertools:$PYTHONPATH
+export PYTHONPATH=$LVSR:$LVSR/libs/blocks:$LVSR/libs/blocks-extras:$LVSR/libs/fuel:$LVSR/libs/Theano:$LVSR/libs/picklable-itertools:$PYTHONPATH
 export PATH=$LVSR/libs/blocks/bin:$LVSR/libs/fuel/bin:$PATH
