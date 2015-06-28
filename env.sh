b61716e0e855a8d3661b48ed8fbf7564b15bc392
Jan Chorowski <jan.chorowski@gmail.com>
2015-06-28 13:18:21 +0200

    blocks-extras/bin in env.sh

--- env.sh
+++ env.sh
@@ -5,4 +5,4 @@ export LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #python modules
 export PYTHONPATH=$LVSR:$LVSR/libs/blocks:$LVSR/libs/blocks-extras:$LVSR/libs/fuel:$LVSR/libs/Theano:$LVSR/libs/picklable-itertools:$PYTHONPATH
-export PATH=$LVSR/libs/blocks/bin:$LVSR/libs/fuel/bin:$PATH
+export PATH=$LVSR/libs/blocks/bin:$LVSR/libs/blocks-extras/bin:$LVSR/libs/fuel/bin:$PATH
