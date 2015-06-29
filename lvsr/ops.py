e070caee5d00eef74455c1c55f51d35239b79864
serdyuk <serdyuk.dmitriy@gmail.com>
2015-06-29 16:30:25 -0400

    Add Op for next probabilities

--- lvsr/ops.py
+++ lvsr/ops.py
@@ -1,4 +1,5 @@
 import fst
+import numpy
 import theano
 from theano import Op
 from fuel.utils import do_not_pickle_attributes
@@ -20,7 +21,11 @@ class FST(object):
         return self.fst.keys()
 
 
-class FSTApplyOp(Op):
+class FSTTransitionOp(Op):
+    """Performs transition in an FST.
+
+    Given a state and an input symbol (character) returns the next state and
+    the output symbol (word)."""
     __props__ = ()
 
     def __init__(self, fst, symbol_table):
@@ -34,8 +39,8 @@ class FSTApplyOp(Op):
         arcs = {arc.ilabel:arc for arc in self.fst[state]}
         if int(input) in arcs:
             arc = arcs[int(input)]
-        elif self.misambig_symbol in arcs:
-            arc = arcs[self.misambig_symbol]
+        elif self.disambig_symbol in arcs:
+            arc = arcs[self.disambig_symbol]
         else:
             raise ValueError
         new_state[0] = arc.nextstate
@@ -48,3 +53,27 @@ class FSTApplyOp(Op):
         state = theano.tensor.as_tensor_variable(state)
         input = theano.tensor.as_tensor_variable(input)
         return theano.Apply(self, [state, input], [state.type(), input.type()])
+
+
+class FSTProbabilitiesOp(Op):
+    """Returns transition log probabilities for all possible input symbols."""
+    __props__ = ()
+
+    def __init__(self, fst, symbol_table):
+        self.fst = fst
+        self.symbol_table = symbol_table
+        self.max_prob = 1e+12
+
+    def perform(self, node, inputs, output_storage):
+        state, = inputs
+        arcs = {arc.ilabel:arc for arc in self.fst[state]}
+        logprobs = numpy.ones(self.symbol_table) * self.max_prob
+        for i, name in enumerate(self.symbol_table):
+            logprobs[i] = arcs[name].weight
+        output_storage[0][0] = logprobs
+
+    def make_node(self, state, input):
+        # check that the theano version has support for __props__
+        assert hasattr(self, '_props')
+        state = theano.tensor.as_tensor_variable(state)
+        return theano.Apply(self, [state], [theano.tensor.vector()])
