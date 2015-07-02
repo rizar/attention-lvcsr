357cdb5b6413afa26513bf99560187fa0a8b7e8d
serdyuk <serdyuk.dmitriy@gmail.com>
2015-07-02 14:06:32 -0400

    Divided for loop into three

--- bin/create_lexicon.py
+++ bin/create_lexicon.py
@@ -2,8 +2,6 @@
 import sys
 
 def main(filename):
-    data_started = False
-    one_gram_started = False
     chars = {'<eps>': 0, '<spc>': 1, '#0': 2, '#1': 3}
     with open("relabel.txt", "w") as f:
         f.write("3 0\n")
@@ -15,29 +13,35 @@ def main(filename):
 
         f_words.write("<eps> 0\n<UNK> 1\n</s> 2\n<s> 3\n<spc> 4\n#0 5\n")
         n_words = 6
+
+        # Step 1: Wait until we read \data
         for line in f:
-            # Step 3: Read all 1-grams
-            if one_gram_started:
-                if line.strip() == "":
-                    break
-                _, word, _ = line.split()
-                if word.startswith("<") or word.startswith("#"):
-                    continue
-                f_words.write("{} {}\n".format(word.strip(), n_words))
-                n_words += 1
-
-                f_lexicon.write("{} {}\n".format(word, " ".join(word)))
-                for char in word:
-                    if char not in chars:
-                        chars[char] = len(chars)
-                        f_characters.write("{} {}\n".format(char, chars[char]))
-
-            # Step 1: Wait until we read \data
             if line.startswith("\\data"):
-                data_started = True
-            # Step 2: Wait until we read \1-gram
-            if line.startswith("\\1-gram") and data_started:
-                one_gram_started = True
+                break
+
+        # Step 2: Wait until we read \1-gram
+        for line in f:
+            if line.startswith("\\1-gram"):
+                break
+
+        # Step 3: Read all 1-grams
+        for line in f:
+            if line.strip() == "":
+                continue
+            if line.startswith('\\2-grams') or line.startswith('\\end'):
+                break
+            _, word, _ = line.split()
+            if word.startswith("<") or word.startswith("#"):
+                continue
+            f_words.write("{} {}\n".format(word.strip(), n_words))
+            n_words += 1
+
+            f_lexicon.write("{} {}\n".format(word, " ".join(word)))
+            for char in word:
+                if char not in chars:
+                    chars[char] = len(chars)
+                    f_characters.write("{} {}\n".format(char, chars[char]))
+
 
 
 if __name__ == '__main__':
