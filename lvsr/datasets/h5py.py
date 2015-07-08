dda082d567f432379519aa96e763787816ba832d
serdyuk <serdyuk.dmitriy@gmail.com>
2015-07-08 11:53:47 -0400

    Fixed CER computation

--- lvsr/datasets/h5py.py
+++ lvsr/datasets/h5py.py
@@ -13,7 +13,7 @@ class H5PYAudioDataset(H5PYDataset):
         self.eos_label = self.char2num['<eol>']
 
     def decode(self, labels):
-        return "".join(self.num2char[label] for label in labels
-                       if label != self.eos_label)
+        return [self.num2char[label] for label in labels
+                if label != self.eos_label]
 
 
