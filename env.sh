619a1e4f94b6f17ebf12d6a3534c9f8ea365856a
Jan Chorowski <jan.chorowski@gmail.com>
2015-08-03 16:39:01 +0200

    Add local configuration for key libraries

--- env.sh
+++ env.sh
@@ -9,6 +9,9 @@ then
     export LD_LIBRARY_PATH=$KALDI_ROOT/tools/openfst/lib:$LD_LIBRARY_PATH
 fi
 
+export BLOCKS_CONFIG=$LVSR/config/blocks.yaml
+export THEANORC=$LVSR/config/theano.rc:$HOME/.theanorc
+
 #python modules
 export PYTHONPATH=$LVSR:$LVSR/libs/blocks:$LVSR/libs/blocks-extras:$LVSR/libs/fuel:$LVSR/libs/Theano:$LVSR/libs/picklable-itertools:$PYTHONPATH
 export PATH=$LVSR/bin:$LVSR/libs/blocks/bin:$LVSR/libs/blocks-extras/bin:$LVSR/libs/fuel/bin:$PATH
