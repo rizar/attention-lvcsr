02852ab66c3f6fabcf8f4b8804558855f1706fb2
rizar <dimabv@tut.by>
2015-08-10 08:43:16 +0000

    Fix configuration comment

--- lvsr/configs/wsj_bhd10.bash
+++ lvsr/configs/wsj_bhd10.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# like jan_baseline but with smaller initial weights
+# like bhd8 but with no regularization at the second stage
 $LVSR/lvsr/run.py --params wsj_bhd8.zip train wsj_bhd10r.zip $LVSR/lvsr/configs/wsj_bhd4.yaml\
     regularization.max_norm 0 net.prior.type "'window_around_median'"
