90b998c4438a89585750940ed38072c2761d19c0
serdyuk <serdyuk.dmitriy@gmail.com>
2015-09-08 17:08:59 -0400

    Fix command line arguments

--- lvsr/main.py
+++ lvsr/main.py
@@ -625,18 +625,18 @@ def train(config, cmd_args):
     validation = DataStreamMonitoring(
         attach_aggregation_schemes([cost, weights_entropy, weights_penalty]),
         data.get_stream("valid"), prefix="valid",
-        before_first_epoch=not cmd_args.fast_start,
-        every_n_epochs=cmd_args.validation_epochs,
-        every_n_batches=cmd_args.validation_batches, after_training=False)
+        before_first_epoch=not cmd_args['fast_start'],
+        every_n_epochs=cmd_args['validation_epochs'],
+        every_n_batches=cmd_args['validation_batches'], after_training=False)
     extensions.append(validation)
     recognizer.init_beam_search(10)
     per = PhonemeErrorRate(recognizer, data.get_dataset("valid"))
     per_monitoring = DataStreamMonitoring(
         [per], data.get_stream("valid", batches=False, shuffle=False),
         prefix="valid",
-        before_first_epoch=not cmd_args.fast_start,
-        every_n_epochs=cmd_args.per_epochs,
-        every_n_batches=cmd_args.per_batches,
+        before_first_epoch=not cmd_args['fast_start'],
+        every_n_epochs=cmd_args['per_epochs'],
+        every_n_batches=cmd_args['per_batches'],
         after_training=False)
     extensions.append(per_monitoring)
     track_the_best_per = TrackTheBest(
