718c0af2b42b69d5d1857bf93b63ef58b8ba690d
dima <serdyuk.dmitriy@gmail.com>
2015-09-10 23:52:12 +0400

    Unify help messages

--- lvsr/run.py
+++ lvsr/run.py
@@ -40,11 +40,11 @@ if __name__ == "__main__":
         # very strange with nargs. I wasn't able to place config changes
         # at the end.
         parser.add_argument(
-            "config_path", help="The configuration")
+            "config_path", help="The configuration path")
 
     train_parser.add_argument(
         "save_path", default="chain",
-        help="The path to save the training process.")
+        help="The path to save the training process")
     train_parser.add_argument(
         "--bokeh-name", default="", type=str,
         help="Name for Bokeh document")
@@ -56,42 +56,42 @@ if __name__ == "__main__":
         help="Load the log from a separate pickle")
     train_parser.add_argument(
         "--fast-start", default=False, action="store_true",
-        help="Skip initial validation cost and PER computatoins.")
+        help="Skip initial validation cost and PER computatoins")
     train_parser.add_argument(
         "--test-tag", default=None, type=int,
         help="Tag the batch with test data for debugging?")
     train_parser.add_argument(
         "--validation-batches", type=int, default=0,
-        help="Perform validation every n batches. `Inf` is acceptable")
+        help="Perform validation every n batches")
     train_parser.add_argument(
         "--validation-epochs", type=int, default=1,
-        help="Perform validation every n epochs. `Inf` is acceptable")
+        help="Perform validation every n epochs")
     train_parser.add_argument(
         "--per-batches", type=int, default=0,
-        help="Perform validation of PER every n batches. `Inf` is acceptable")
+        help="Perform validation of PER every n batches")
     train_parser.add_argument(
         "--per-epochs", type=int, default=2,
-        help="Perform validation of PER every n epochs. `Inf` is acceptable")
+        help="Perform validation of PER every n epochs")
 
     search_parser.add_argument(
         "--part", default="valid",
-        help="Data to recognize with beam search.")
+        help="Data to recognize with beam search")
     search_parser.add_argument(
         "--beam-size", default=10, type=int,
         help="Beam size")
     search_parser.add_argument(
         "--char-discount", default=0.0, type=float,
         help="A discount given by beam search for every additional character"
-        " added to a candidate.")
+        " added to a candidate")
     search_parser.add_argument(
         "--old-labels", default=False, action="store_true",
-        help="Expect old labels when decoding.")
+        help="Expect old labels when decoding")
     search_parser.add_argument(
         "--report", default=None,
-        help="Destination to save a detailed report.")
+        help="Destination to save a detailed report")
     search_parser.add_argument(
         "--decoded-save", default=None,
-        help="Destination to save decoded sequences.")
+        help="Destination to save decoded sequences")
     search_parser.add_argument(
         "--decode-only", default=None,
         help="Only decode the following utternaces")
@@ -102,7 +102,7 @@ if __name__ == "__main__":
                    show_data_parser, search_parser]:
         parser.add_argument(
             "config_changes", default=[], nargs='*',
-            help="Changes to configuration. Path, value, path, value.")
+            help="Changes to configuration. [<path>, <value>]")
 
     root_parser.add_argument(
         "--logging", default='INFO', type=str,
