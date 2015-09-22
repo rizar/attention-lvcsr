a6e75eb069edea83ee7a293fcc5f043c7d04a992
serdyuk <serdyuk.dmitriy@gmail.com>
2015-09-22 15:49:19 -0400

    Split beam search and scoring with kaldi

---
+++ exp/wsj/score.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+KU=$KALDI_ROOT/egs/wsj/s5/utils
+KL=$KALDI_ROOT/egs/wsj/s5/local
+
+. $KU/parse_options.sh
+
+if [ $# -ne 2 ]; then
+	echo "usage: `basename $0` <dir> <part>"
+	echo "options:"
+	echo "		--part name					#partition to score"
+	echo "		--beam_serch_opts 'opts'	#opts passed to beam search"
+	exit 1
+fi
+
+dir=$1
+part=$2
+
+# Aggregate groundtruth
+cat $dir/$part-groundtruth-text.txt | sort | $KL/wer_ref_filter > $dir/tmp
+mv $dir/tmp $dir/$part-groundtruth-text.txt
+
+# Aggregate decoded
+$LVSR/bin/decoded_chars_to_words.py $lexicon $dir/$part-decoded.out - | $KL/wer_hyp_filter > $dir/$part-decoded-text.out
+
+# Score
+compute-wer --text --mode=all ark:$dir/$part-groundtruth-characters.txt ark:$dir/$part-decoded.out $dir/$part-characters.errs > $dir/$part-characters.wer
+compute-wer --text --mode=all ark:$dir/$part-groundtruth-text.txt ark:$dir/$part-decoded-text.out $dir/$part-text.errs > $dir/$part-text.wer
