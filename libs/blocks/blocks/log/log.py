3c649da75b3af56fdd455f9cc3901a6d9702d587
Jan Chorowski <jan.chorowski@gmail.com>
2016-01-10 10:56:17 +0100

    fixes to ndarraylog + pandas conversion

--- libs/blocks/blocks/log/log.py
+++ libs/blocks/blocks/log/log.py
@@ -9,6 +9,7 @@ import array
 import collections
 from bisect import bisect_left
 import numpy
+import pandas
 
 
 @six.add_metaclass(ABCMeta)
@@ -218,7 +219,7 @@ class ListLog(TrainingLogBase):
 class _TimeSlice(collections.Mapping):
     def __init__(self, time, log):
         self._time = time
-        self._columns = log.columns
+        self._columns = log._columns
         assert isinstance(self._columns, OrderedDict)
 
     def __getitem__(self, item):
@@ -236,8 +237,8 @@ class _TimeSlice(collections.Mapping):
         for k, ndarr in self._columns.iteritems():
             times = ndarr['idx']
             idx = times.searchsorted(time)
-            if idx < times.shape[0] and times['idx'] == time:
-                return k
+            if idx < times.shape[0] and times[idx] == time:
+                yield k
 
     def __len__(self):
         l = 0
@@ -245,7 +246,7 @@ class _TimeSlice(collections.Mapping):
         for ndarr in self._columns.itervalues():
             times = ndarr['idx']
             idx = times.searchsorted(time)
-            if idx < times.shape[0] and times['idx'] == time:
+            if idx < times.shape[0] and times[idx] == time:
                 l += 1
         return l
 
@@ -268,51 +269,77 @@ class NDarrayLog(TrainingLogBase):
         return DTYPES.get(type(obj), numpy.dtype('object'))
 
     def __init__(self):
-        self.columns = OrderedDict()
-        self.col_tops = {}
+        self._columns = OrderedDict()
+        self._col_tops = {}
         self.status = {}
-        self.current_time = -1
-        self.current_dict = None
+        self._current_time = -1
+        self._current_dict = None
         TrainingLogBase.__init__(self)
 
     def __getitem__(self, time):
         self._check_time(time)
-        if time == self.current_time:
-            return self.current_dict
-        elif time > self.current_time:
+        if time == self._current_time:
+            return self._current_dict
+        elif time > self._current_time:
             # Append the last value to column arrays
-            if self.current_time >= 0:
-                for k, v in self.current_dict.iteritems():
-                    if k in self.columns:
-                        col = self.columns[k]
-                        idx = self.col_tops[k]
-                        self.col_tops[k] = idx + 1
+            if self._current_time >= 0:
+                for k, v in self._current_dict.iteritems():
+                    if k in self._columns:
+                        col = self._columns[k]
+                        idx = self._col_tops[k]
+                        self._col_tops[k] = idx + 1
                         if idx >= col.shape[0]:
                             col2 = numpy.empty((1.3 * idx), col.dtype)
                             col2[:idx] = col
                             col2[idx:]['idx'] = 2147483647
                             col = col2
-                            self.columns[k] = col2
-                        col['idx'][idx] = self.current_time
+                            self._columns[k] = col2
+                        col['idx'][idx] = self._current_time
                         col['val'][idx] = v
                     else:
-                        self.columns[k] = numpy.empty(
+                        self._columns[k] = numpy.empty(
                             (10,),
                             dtype=[('idx', numpy.int32),
                                    ('val', self.get_dtype(v))])
-                        self.columns[k]['idx'][:] = 2147483647
-                        self.columns[k]['idx'][0] = self.current_time
-                        self.columns[k]['val'][0] = v
-                        self.col_tops[k] = 1
-            self.current_time = time
-            self.current_dict = {}
-            return self.current_dict
+                        self._columns[k]['idx'][:] = 2147483647
+                        self._columns[k]['idx'][0] = self._current_time
+                        self._columns[k]['val'][0] = v
+                        self._col_tops[k] = 1
+            self._current_time = time
+            self._current_dict = {}
+            return self._current_dict
         else:
             return _TimeSlice(time, self)
 
     def __setitem__(self, time, value):
         self._check_time(time)
-        if time == self.current_time:
-            self.current_dict = value
+        if time == self._current_time:
+            self._current_dict = value
         else:
             raise KeyError("Can't modify log entries for the past")
+
+    def to_pandas(self):
+        """
+        Return a pandas DataFrame view of the log.
+
+        """
+        # Write down the last record
+        if self._current_dict:
+            # Executes if self._current_dict has uncommitted chages
+            unused_dict = self[self._current_time + 1]
+        series = {}
+        for name, col in self._columns.iteritems():
+            col = col[:self._col_tops[name]]
+            col.setflags(write=False)
+            print name
+            if col['val'].ndim == 1:
+                dtype = col['val'].dtype
+                data = col['val']
+            else:
+                dtype = 'object'
+                data = list(col['val'])
+            s = pandas.Series(data, index=col['idx'], dtype=dtype)
+            series[name] = s
+        # this makes a copy of all the data :(
+        # TODO: how to directly get a sparse DataFrame??
+        return pandas.DataFrame(series)
