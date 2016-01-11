238b3dd95bc8630fa16f17be8a94e23c6a55ea92
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2016-01-11 11:37:06 -0500

    Fix scoring
    Kaldi script compute-wer doesn't support error printing anymore

--- exp/wsj/score.sh
+++ exp/wsj/score.sh
@@ -30,5 +30,5 @@ $LVSR/exp/wsj/extract_for_kaldi.sh $report | sed -e 's/<noise>/<NOISE>/g' | \
 $KL/wer_hyp_filter $dir/tmp > $dir/$part-decoded-text.out
 
 # Score
-compute-wer --text --mode=all ark:$dir/$part-groundtruth-text.txt ark:$dir/$part-decoded-text.out $dir/$part-text.errs > $dir/$part-text.wer
+compute-wer --text --mode=all ark:$dir/$part-groundtruth-text.txt ark:$dir/$part-decoded-text.out > $dir/$part-text.wer
 cat $dir/$part-text.wer
